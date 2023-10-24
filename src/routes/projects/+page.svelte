47f578501a645ef042b7851563122df7a63e5464
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-10-24 15:07:57 +0400

    projectschanged

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,15 +3,14 @@
 </script>
 
 <svelte:head>
-	<title>Gianmarco Cavallo — Projects</title>
+	<title>keto— Projects</title>
 </svelte:head>
 
 <div class="projectContainer">
 	<div class="projects">
 		<h1>Projects</h1>
 		<p class="note">
-			Note: most of my projects are under NDA so I am unable to share those projects. If there is
-			anything you would like to see further, please feel free to contact me.
+			notebi proektebis.
 		</p>
 		{#each projects as project}
 			<div class="project">
